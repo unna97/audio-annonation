ba3ef9c513f518db431a72a93bfdb01519d06250
unna97 <31486108+unna97@users.noreply.github.com>
2023-11-25 18:10:16 +0530

    Refactor serializers and views in the API module
    - Remove previous experimental apis defination and related
    - Implement CRUD for the AudioFile model. Show API views for all
    - Implement AudioFileList View as well

--- api/serializers.py
+++ api/serializers.py
@@ -1,26 +1,10 @@
 # api/serializers.py
 from rest_framework import serializers
-from waveform_audio.models import AudioAnnotation, AudioFile
+from waveform_audio.models import AudioFile
 
-class AudioAnnotationSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = AudioAnnotation
-        fields = ('id', 'audio_file', 'annotation', 'start_time', 'end_time', 'timestamp')
-
-    def create(self, validated_data):
-        return AudioAnnotation.objects.create(**validated_data)
-
-class AudioFileSerializer(serializers.ModelSerializer):
-
+class AudioFileSerializer(serializers.HyperlinkedModelSerializer):
+  
     class Meta:
         model = AudioFile
         fields = ('id', 'file', 'timestamp')
-    
-    def create(self, validated_data):
-        
-        return AudioFile.objects.create(**validated_data)
 
-    
-
-    
