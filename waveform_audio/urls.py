f0e93d416701849632cd061a451f9f0a86fcbf16
unna97 <31486108+unna97@users.noreply.github.com>
2023-11-30 15:13:44 +0530

    Change function to class based view for index
    - Change function to class  based views and reference audio files
    - Reference Audio files with ids
    - Change index.html and urls for to account for above changes

--- waveform_audio/urls.py
+++ waveform_audio/urls.py
@@ -23,10 +23,15 @@ app_name = "waveform_audio"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index_view, name="index"),
+    
+    
+    path("", views.AudioFileAvailableView.as_view(), name="index"),
+
     path("annotate/", views.annotate_view, name="annotate"),
     path("save_annotations/", views.save_annotations, name="save_annotations"),
     path("update_database/", views.update_database, name="update_database"),
     path("clean_database/", views.clean_database, name="clean_database"),
+    # example url:
+    # API urls:
     path("api/", include("api.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
