321fac03699bb2feb2863bff3dd9e999039ac1f4
unna97 <31486108+unna97@users.noreply.github.com>
2023-12-07 13:49:59 +0530

    ✨ Add AudioAnnotations related API (list + by id)

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('audio-files/', views.AudioFileListAPIView.as_view(), name='audio-file-list'),
     path('audio-files/<int:pk>/', views.AudioFileDetailAPIView.as_view(), name='audio-file-detail'),
     path('audio-files/upload/', views.AudioFileUploadAPIView.as_view(), name='audio-file-upload'),
+    path('audio-files/<int:audio_file_id>/annotations/', views.AudioAnnotationListAPIView.as_view(), name='audio-annotation-list'),
+    path('audio-files/<int:audio_file_id>/annotations/<int:pk>/', views.AudioAnnotationDetailAPIView.as_view(), name='audio-annotation-detail'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from .serializers import *
 from rest_framework import generics, status
-from waveform_audio.models import AudioFile
+from waveform_audio.models import AudioFile, AudioAnnotation
 from rest_framework.response import Response
 import os
 
@@ -9,6 +9,7 @@ import os
 class AudioFileDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = AudioFile.objects.all()
     serializer_class = AudioFileSerializer #use HyperlinkedModelSerializer to get url field
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
 
@@ -29,6 +30,17 @@ class AudioFileDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
         # Respond with a success status
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+    def options(self, request, *args, **kwargs):
+        """
+        Don't include the view description in OPTIONS responses.
+        """
+        meta = self.metadata_class()
+        data = meta.determine_metadata(request, self)
+        data.pop('description')
+        return Response(data=data, status=status.HTTP_200_OK)        
+        
+
 class AudioFileUploadAPIView(generics.CreateAPIView):
     serializer_class = AudioFileSerializer
     queryset = AudioFile.objects.all()
@@ -36,3 +48,32 @@ class AudioFileUploadAPIView(generics.CreateAPIView):
 class AudioFileListAPIView(generics.ListAPIView):
     serializer_class = AudioFileSerializer
     queryset = AudioFile.objects.all()
+
+
+class AudioAnnotationListAPIView(generics.ListAPIView):
+    serializer_class = AudioAnnotationSerializer
+    queryset = AudioAnnotation.objects.all()
+    lookup_field = "audio_file_id"
+
+    def options(self, request, *args, **kwargs):
+        return super().options(request, *args, **kwargs)
+    
+
+    def get_queryset(self):
+        audio_file_id = self.kwargs['audio_file_id']
+        return AudioAnnotation.objects.filter(audio_file_id=audio_file_id)
+    
+# Allow user to create , update, view and delete audio annotation by id of the annotation
+class AudioAnnotationDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = AudioAnnotation.objects.all()
+    serializer_class = AudioAnnotationSerializer
+    # lookup_field = "id"
+
+    def options(self, request, *args, **kwargs):
+        """
+        Don't include the view description in OPTIONS responses.
+        """
+        meta = self.metadata_class()
+        data = meta.determine_metadata(request, self)
+        data.pop('description')
+        return Response(data=data, status=status.HTTP_200_OK)
