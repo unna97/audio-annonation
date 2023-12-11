bed72ae9722707e60362128bb57c97d68d18e6ec
unna97 <31486108+unna97@users.noreply.github.com>
2023-12-11 17:35:06 +0530

    Make upload a django crispy form

--- waveform_audio/forms.py
+++ waveform_audio/forms.py
@@ -1,5 +1,7 @@
 # import crispy_forms.helper as crispy_helper
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
 
 # url: https://stackoverflow.com/questions/24783275/django-form-with-choices-but-also-with-freetext-option?noredirect=1&lq=1
@@ -8,9 +10,17 @@ class AudioFileForm(forms.Form):
         label="Select a file",
         help_text="insert an audio file",
         widget=forms.FileInput(attrs={"accept": "audio/*"}),
+
     )
     artist_names = forms.CharField(
         label="Artist name(s)",
         help_text="insert artist name(s)",
         widget=forms.TextInput(attrs={"placeholder": "artist name(s)"}),
     )
+
+    @property
+    def helper(self):
+        helper = FormHelper()
+        helper.form_method = "POST"
+        helper.inputs.append(Submit("submit", "Submit"))
+        return helper
