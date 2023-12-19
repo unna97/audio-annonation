3ec12c2379007ce0703a2d54570dc7b8314c9717
Christopher Keim <122534665+christopherkeim@users.noreply.github.com>
2023-12-18 22:45:32 -0500

    Formatting with black.

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_NAME = os.getenv('DB_NAME', 'waveform_audio')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv("DB_NAME", "waveform_audio")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
