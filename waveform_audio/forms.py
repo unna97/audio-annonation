15c15dd609b26371d8cd2b16f486450eb70a948a
unna97 <31486108+unna97@users.noreply.github.com>
2024-08-29 00:13:01 +0530

    - Refactor UploadAudio & Subtitle View
    * Add message alert for errors as well as success
    * Make upload atomic
    * Shift subtitle data save logic to form

--- waveform_audio/forms.py
+++ waveform_audio/forms.py
@@ -1,8 +1,9 @@
 # import crispy_forms.helper as crispy_helper
 from django import forms
-from .models import AudioFile
+from .models import AudioFile, Subtitle
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit  # Layout, Row, Column
+from crispy_forms.layout import Submit
+from . import utils
 
 
 # url: https://stackoverflow.com/questions/24783275/django-form-with-choices-but-also-with-freetext-option?noredirect=1&lq=1
@@ -54,3 +55,26 @@ class SubtitleFileForm(forms.Form):
         helper.form_method = "POST"
         helper.inputs.append(Submit("submit", "Submit"))
         return helper
+
+    def clean_subtitle_file(self):
+        subtitle_file = self.cleaned_data.get("subtitle_file")
+        if subtitle_file:
+            try:
+                self.subtitle_texts = utils.process_subtitle_file(subtitle_file)
+            except Exception as e:
+                raise forms.ValidationError(f"Error processing subtitle file: {str(e)}")
+        return subtitle_file
+
+    def save(self, audio_file_instance):
+        if hasattr(self, "subtitle_texts"):
+            subtitles = [
+                Subtitle.objects.create(
+                    audio_file=audio_file_instance,
+                    start_time=subtitle["start_time"],
+                    end_time=subtitle["end_time"],
+                    content=subtitle["content"],
+                )
+                for subtitle in self.subtitle_texts
+            ]
+            return subtitles
+        return []
